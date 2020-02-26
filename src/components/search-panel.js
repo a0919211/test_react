83f9905dd379d79edd6b74b0348fd000878fef0a
Administrator <aaa144855@gmail.com>
2020-02-26 22:56:54 +0300

    todo sample

---
+++ src/components/search-panel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchPanel = () => {
+    return (
+        <input
+            placeholder={"123"}
+            autoComplete="123" />
+    )
+};
+
+export default SearchPanel;
